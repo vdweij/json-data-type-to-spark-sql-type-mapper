addb13a2d385abfdccbb7524ca27579fb70b00c1
Aede van der Weij <vdweij@gmail.com>
2023-11-12 21:20:46 +0100

    Prepares for release to testpypi

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 from setuptools import setup, find_packages
+from datetime import datetime
+
+# Generate a timestamp for the version
+timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+version_postfix = '.dev.' + timestamp
 
 # Read the requirements from requirements.txt file
 with open('requirements.txt') as f:
@@ -6,7 +11,7 @@ with open('requirements.txt') as f:
 
 setup(
     name='json2spark_mapper',
-    version='0.0.1',
+    version='0.0.1' + version_postfix,
     description='Maps JSON schema types to Spark SQL types',
     #author='',
     #author_email='',
